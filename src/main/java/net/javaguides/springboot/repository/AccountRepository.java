fa8f6a38046400a4271fd06620a0d9a762530aae
Ashwini Nikam <ashu.nikam26@gmail.com>
2023-09-20 12:58:25 -0700

    msg

---
+++ src/main/java/net/javaguides/springboot/repository/AccountRepository.java
@@ -0,0 +1,16 @@
+package net.javaguides.springboot.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import net.javaguides.springboot.model.Account;
+import net.javaguides.springboot.model.Appointment;
+
+
+@Repository
+public interface AccountRepository extends JpaRepository<Account, Long>{
+	List<Account> findByType(String type);
+	
+}
